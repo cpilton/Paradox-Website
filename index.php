6bef6ff980db783c54c35e17fa7bfdd2f786fd88
Callum <pilton.callum@gmail.com>
2020-08-10 17:39:57 +0100

    Update links

--- index.php
+++ index.php
@@ -1,1 +1,53 @@
-<?php include_once(“index.html”); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <link rel="icon" href="img/logo.ico"/>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="description" content="Paradox Privacy Extension">
+    <meta name="keywords" content="privacy, paradox, chrome, extension, google, privacy policy, analysis, trackers, cookies, CORS, local storage">
+    <meta name="author" content="Callum Pilton">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paradox</title>
+</head>
+<body>
+<div id="top">
+    <div class="logo" id="top-logo" onclick="window.location.href='/'"></div>
+    <div id="links">
+        <a class="link" href="https://chrome.google.com/webstore/detail/paradox/mbdalgoachccbfodfghhdgaepgkdaoke" target="_blank">Get Paradox</a>
+        <a class="link" href="using-paradox.html">Using Paradox</a>
+        <a class="link" href="privacy-policy.html">Privacy Policy</a>
+    </div>
+</div>
+<div id="segment1" class="segment">
+    <div class="container">
+        <div class="logo" id="main-logo"></div>
+        <div class="title">PARADOX</div>
+        <div class="subtitle">Helping People make Privacy Decisions</div>
+        <div id="get-paradox" onclick="window.open('https://chrome.google.com/webstore/detail/paradox/mbdalgoachccbfodfghhdgaepgkdaoke')">Get Paradox</div>
+        <div class="normal">Paradox helps users make privacy-based decisions by checking and comparing tracking methods
+            against the privacy policy of the websites you visit, giving you complete transparency of use. We put you in
+            control of making your own privacy decisions.
+        </div>
+    </div>
+</div>
+
+<div id="segment2" class="segment">
+    <div class="container">
+        <div class="heading">The Paradox extension both displays data being collected on you in real-time, and analyses privacy policies to put you in control of making informed privacy decisions. </div>
+        <div class="img-container" id="extension-container">
+            <div class="image" id="extension-img"></div>
+            <div class="heading">Paradox Extension</div>
+            <div class="standard">The Paradox Extension summarises the personal data being collected by the websites you visit, and compares this to the privacy policy; empowering you to make better privacy decisions</div>
+        </div>
+        <div class="img-container" id="popup-container">
+            <div class="image" id="popup-img"></div>
+            <div class="heading">Paradox Popup</div>
+            <div class="standard">The Paradox Popup provides a summary of the Privacy Policy of websites you visit, and pops up whenever you create an account on a new website</div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
